b84b09bbb7fdb46281af0f6a2459e59031787348
Vivek Aanand Ganesan <vivganes@gmail.com>
2013-06-09 17:55:55 -0700

    Updated mongo loader with CLI params

--- mongo_loader.rb
+++ mongo_loader.rb
@@ -14,8 +14,8 @@ class MovieLenser
 
   attr_reader :db
 
-  def initialize(file_path)
-    @client = MongoClient.new('localhost', 27017)
+  def initialize(file_path, host="localhost", port=27017)
+    @client = MongoClient.new(host, port)
     @db     = @client['movielensdb']
     @ratings = {}
     fh = File.open(file_path)
@@ -40,17 +40,17 @@ class MovieLenser
   def iter_and_save
     good_count = 0
     @ratings.keys.sort_by(&:to_i).each do |user_id|
-      rating = @ratings[user_id]
-      if rating.nil? then
+      user_ratings = @ratings[user_id]
+      if user_ratings.nil? then
         puts "Missing data for user '#{user_id}'"
-      elsif rating.keys.size < 20 then
-        puts "Only '#{rating.keys.size}' ratings for user '#{user_id}'"
+      elsif user_ratings.keys.size < 20 then
+        puts "Only '#{user_ratings.keys.size}' ratings for user '#{user_id}'"
       else
         good_count += 1
         user_rec = {"_id" => user_id, :ratings => []}
         rating_sum = 0
         rating_count = 0
-        rating.keys.sort_by(&:to_i).each do |movie_id|
+        user_ratings.keys.sort_by(&:to_i).each do |movie_id|
           # #### #
           # Save the movie record
           # #### #
@@ -63,18 +63,18 @@ class MovieLenser
           # Save the ratings
           # #### #
           rating_count += 1
-          rating_sum += rating[movie_id].to_i
+          rating_sum += user_ratings[movie_id].to_i
           rating_rec = {}
           rating_rec[:user_id] = user_id
           rating_rec[:movie_id] = movie_id 
-          rating_rec[:rating] = rating[movie_id]
+          rating_rec[:rating] = user_ratings[movie_id]
           @db["ratings"].insert(rating_rec)
           # #### #
           # Save the user record
           # #### #
           user_rec[:ratings] << {
             :movie_id => movie_id,
-            :rating => rating[movie_id]
+            :rating => user_ratings[movie_id]
           }
         end
         user_rec[:num_ratings] = rating_count
@@ -82,7 +82,7 @@ class MovieLenser
           user_rec[:avg_rating] = (rating_sum.to_f/rating_count.to_f)
         end
         @db["users"].insert(user_rec)
-        #puts "#{Time.now} User #{user_id} done, #movies = #{rating_count}"
+        puts "#{Time.now} User #{user_id} done, #movies = #{rating_count}"
       end
     end
     puts "Count : #{@db["ratings"].count}"
@@ -93,10 +93,11 @@ end
 # ########################################################################## #
 # Main section
 # ########################################################################## #
-raise "Missing file name\nUsage : #{$0} <file name>" unless ARGV[0]
+usg_str = "Usage : #{$0} <u.data file path> <mongodb host> <mongodb port>"
+raise "Missing file name\n#{usg_str}" unless ARGV[0]
 
 starting_ts = Time.now
-movie_lenser = MovieLenser.new(ARGV[0])
+movie_lenser = MovieLenser.new(ARGV[0], ARGV[1], ARGV[2])
 movie_lenser.clean
 movie_lenser.iter_and_save
 finished_ts = Time.now
